0d914589448f6c37ca14923a197aadb4d216d57a
Walter Kiprono <walterkiprono81@gmail.com>
2024-05-21 01:10:07 +0300

    chore: add content to Payment Methods

--- src/pages/PaymentMethods.tsx
+++ src/pages/PaymentMethods.tsx
@@ -1,7 +1,15 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 export default function PaymentMethods() {
     return (
-        <p>test</p>
+        <Box>
+            <Typography variant="h6">
+                Payment Methods
+            </Typography>
+            <Typography variant="body1">We accept payments to the university via M-Pesa.</Typography>
+            <Typography variant="h6">Payment via M-Pesa</Typography>
+            <Typography variant="body1">Instructions for paying through M-Pesa..</Typography>
+        </Box>
     );
 }
